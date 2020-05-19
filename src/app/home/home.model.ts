7c6a685b72c294763a234ca46b3005815554c052
PetchiS <52410883+PetchiS@users.noreply.github.com>
2020-05-19 21:16:06 +0530

    adding src files

---
+++ src/app/home/home.model.ts
@@ -0,0 +1,18 @@
+export class Card {
+    color: string;
+    number: string;
+    text: string;
+}
+export class CardDetails {
+    cardDetails: Card[];
+}
+export class ProductDetails {
+    product: string;
+    createdDate: string;
+    size: string;
+    owner: string;
+    materialStatus: string;
+    delivery: string;
+    approval: string;
+    status: string;
+}
